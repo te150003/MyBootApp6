aefabe6bdfa7ef3fb6570851f9544bd3449000fc
150003 <150003@e0601-01.JC-21.local>
2018-05-16 15:46:48 +0900

    課題6

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/BookBean.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp6;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class BookBean  {
+	Integer id ;
+	String title;
+	String writter;
+	String publisher;
+	Integer price;
+}
