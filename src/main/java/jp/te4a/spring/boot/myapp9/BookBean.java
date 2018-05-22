426a176e06d483c3aff626de8d37bcb6d7662437
150003 <150003@e0601-01.JC-21.local>
2018-05-22 12:59:22 +0900

    課題9

--- src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -1,13 +1,19 @@
-package jp.te4a.spring.boot.myapp8;
+package jp.te4a.spring.boot.myapp9;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
+@Entity
+@Table(name="books")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookBean  {
-	private Integer id ;
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column(nullable = false)
 	private String title;
 	private String writter;
 	private String publisher;
--- src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -1,9 +1,8 @@
-package jp.te4a.spring.boot.myapp8;
-
-import java.util.List;
+package jp.te4a.spring.boot.myapp9;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,22 +13,23 @@ public class BookService{
 	BookRepository bookRepository;
 	
 	public BookForm create(BookForm bookForm) {
-		bookForm.setId(bookRepository.getBookId());
 		BookBean bookBean = new BookBean();
 		BeanUtils.copyProperties(bookForm, bookBean);
-		bookRepository.create(bookBean);
+		bookRepository.save(bookBean);
 		return bookForm;
 	}
 
 	public BookForm update(BookForm bookForm) {
 		BookBean bookBean = new BookBean();
 		BeanUtils.copyProperties(bookForm, bookBean);
-		bookRepository.update(bookBean);
+		bookRepository.save(bookBean);
 		return bookForm;
 	}
 	
 	public void delete(Integer id) {
-		bookRepository.delete(id);
+		BookBean bookBean = new BookBean();
+		bookBean.setId(id);
+		bookRepository.delete(bookBean);
 	}
 	
 	public List<BookForm>findAll(){
@@ -44,9 +44,12 @@ public class BookService{
 	}
 	
 	public BookForm findOne(Integer id) {
-		BookBean bookBean = bookRepository.findOne(id);
+		Optional<BookBean> opt = bookRepository.findById(id);//ここの戻り値をパワポ14参照
 		BookForm bookForm = new BookForm();
-		BeanUtils.copyProperties(bookBean, bookForm);
+		opt.ifPresent(book -> {
+			// bookを使った処理
+			BeanUtils.copyProperties(book, bookForm);
+		});
 		return bookForm;
 	}
 }
